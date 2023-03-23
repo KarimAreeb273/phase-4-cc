46dcac3c03230eba8c45f9a310763c8384720bb8
Areeb Karim <areebakarim99@gmail.com>
2023-03-23 12:12:13 -0400

    done

--- server/app.py
+++ server/app.py
@@ -21,10 +21,62 @@ api = Api(app)
 def index():
     return '<h1>Code challenge</h1>'
 
-@app.route('/restaurants')
-def restaurants():
+class Restaurants(Resource):
+    def get(self):
+        restaurants = Restaurant.query.all()
+        restaurant_dict = [restaurant.to_dict() for restaurant in restaurants]
+        response = make_response(restaurant_dict, 200)
+        return response
 
-    pass
+class RestaurantById(Resource):
+    def get(self, id):
+        restaurants = Restaurant.query.filter(Restaurant.id == id).first()
+        if not restaurants: 
+            return make_response({"error": "Restaurant not found"}, 404)
+        restaurant_dict = restaurants.to_dict(rules=('pizzas',))
+        response = make_response(restaurant_dict, 200)
+        return response
+    
+    def delete(self, id):
+        restaurants = Restaurant.query.filter(Restaurant.id == id).first()
+        if not restaurants: 
+            return make_response({"error": "Restaurant not found"}, 404)
+        db.session.delete(restaurants)
+        db.session.commit()
+        response = make_response({}, 200)
+        return response
+
+class Pizzas(Resource):
+    def get(self):
+        pizzas = Pizza.query.all()
+        pizza_dict = [pizza.to_dict() for pizza in pizzas]
+        response = make_response(pizza_dict, 200)
+        return response
+
+class RestaurantPizzas(Resource):
+    def post(self):
+        new = request.get_json()
+        try:
+            new_pizzas = RestaurantPizza (
+                price = new['price'],
+                pizza_id = new['pizza_id'],
+                restaurant_id = new['restaurant_id']
+            )
+
+            db.session.add(new_pizzas)
+            db.session.commit()
+
+            new_pizzas_dict = new_pizzas.pizzas.to_dict()
+            response = make_response(new_pizzas_dict, 201)
+            return response
+        except ValueError:
+            return make_response ({"error": ["validation errors"]}, 400)
+
+api.add_resource(Restaurants, '/restaurants')
+api.add_resource(RestaurantById, '/restaurant/<int:id>')
+api.add_resource(Pizzas, '/pizzas')
+api.add_resource(RestaurantPizzas, '/restaurant_pizzas')
+        
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -10,4 +10,47 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-# Add models here
+class Pizza(db.Model, SerializerMixin):
+    __tablename__ = 'pizza'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredient = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='pizza')
+
+    serialize_rules = ('-created_at', '-updated_at','-restaurant_pizzas', '-restaurant.pizza')
+
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__ = 'restaurant'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+
+    restaurant_pizzas = db.relationship('RestaurantPizzas', backref='restaurant')
+
+    serialize_rules = ('-restaurant_pizzas', '-pizza.restaurant')
+
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__ = 'restaurant_pizzas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
+
+    serialize_rules = ('-created_at', '-updated_at','-pizza.restaurant_pizzas', '-restaurant.restaurant_pizzas',)
+
+    @validates('price')
+    def validate_price(self, key, price):
+        if not price > 1 and not price < 30:
+            raise ValueError ("Price must be between 1 and 30")
+        return price
+
+
